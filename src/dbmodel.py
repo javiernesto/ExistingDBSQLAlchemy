f5fd90ca67a84d5ccf94505f0545228c452c4948
javiernesto <110693244+javiernesto@users.noreply.github.com>
2025-02-12 17:57:39 -0500

    Added paging logic
    Renamed stored procedure and added paging logic

--- src/dbmodel.py
+++ src/dbmodel.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 from customer import Customer
+import pandas as pd
+
 
 class dbmodel(object):
     db_config = ''
@@ -11,7 +14,7 @@ class dbmodel(object):
 
     def get_customer_name_by_id(self, customer_id):
         Session = sessionmaker(bind=self.engine, future=True)
-        session = Session()
+        session = session()
         result = customer_id
         try:
             customer_row = session.execute(
@@ -21,7 +24,7 @@ class dbmodel(object):
                     Customer.customerid == customer_id
                 )
             ).scalar_one_or_none()
-            
+
             if customer_row is not None:
                 result = customer_row.customername
 
@@ -29,3 +32,30 @@ class dbmodel(object):
         except Exception as ex:
             print(ex.args)
             return customer_id
+
+    def get_clients_by_country_state(
+            self, 
+            country, 
+            state, 
+            page=None, 
+            rowsPerPage=None):
+        df = pd.DataFrame()
+        try:
+            Session = sessionmaker(bind=self.engine, future=True)
+            session = session()
+            query_text = text(
+                "execute dbo.SP_GetClientsByCountryState '{0}', '{1}'".format(country, state) 
+                if page is None or rowsPerPage is None or page < 1 or rowsPerPage < 1 
+                else
+                "execute dbo.SP_GetClientsByCountryState '{0}', '{1}', {2}, {3}".format(country, state, page, rowsPerPage))
+            query = session.execute(query_text)
+            query_columns = query.keys() if query else []
+            data = [dict(zip(query_columns, row)) for row in query.all()]
+
+            # Saving the query data in a dataframe
+            df = pd.DataFrame(data)
+        except Exception as ex:
+            print(ex.args)
+
+        return df
+    
--- src/run.py
+++ src/run.py
@@ -2,17 +2,24 @@ import sys
 import dbmodel as db
 
 def main(argv):
-    user = 'youruser'
-    password = 'yourpassword'
-    server = 'yourhost'
-    dbname = 'yourdbname'
+    user = 'YourUser'
+    password = 'YourPassword'
+    server = 'Server:Port'
+    dbname = 'DataBase'
     connString = 'mssql+pyodbc://{0}:{1}@{2}/{3}?driver=SQL Server'.format(user, password, server, dbname)
 
     accountToGet = 'xxx'
 
     dbSQL = db.dbmodel(connString)
+
+    ### Testing the function get_customer_name_by_id
     customer_name = dbSQL.get_customer_name_by_id(accountToGet)
     print(customer_name)
+
+    ### Testing the function get_clients_by_country_state
+    df_clients = dbSQL.get_clients_by_country_state('USA', 'TX', 1, 20)
+    print(df_clients)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
